f01de8c3300278c2c371e62cb194fdb4839433a8
bantek89 <bannatekle@gmail.com>
2019-06-20 00:24:10 +0100

    get request to api

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
 
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.520"]
-                 [reagent "0.8.1"]]
+                 [reagent "0.8.1"]
+                 [cljs-ajax "0.7.5"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-figwheel "0.5.18"]]
--- src/clojurescript_quiz/core.cljs
+++ src/clojurescript_quiz/core.cljs
@@ -1,6 +1,15 @@
 (ns clojurescript-quiz.core
     (:require
-      [reagent.core :as r]))
+      [reagent.core :as r]
+       [ajax.core :refer [GET]]))
+
+(defn handler [response]
+  (prn (str response)))
+
+(defn error-handler [{:keys [status status-text]}]
+  (prn (str "error: " status " " status-text)))
+
+(GET "https://opentdb.com/api.php?amount=10&category=26&type=multiple")
 
 ;; -------------------------
 ;; Views
