1fba77dbff3cde82c869facfbad263bd02c93ec2
bantek89 <bannatekle@gmail.com>
2019-06-25 01:03:39 +0100

    shuffle implemented

--- src/clojurescript_quiz/core.cljs
+++ src/clojurescript_quiz/core.cljs
@@ -1,16 +1,31 @@
 (ns clojurescript-quiz.core
     (:require
       [reagent.core :as r :refer [atom]]
-       [ajax.core :refer [GET]]))
+       [ajax.core :refer [GET]]
+       [cljss.core :refer-macros [defstyles]]
+       [cljss.core :refer [inject-global]]))
 
 
-; (defstyles component-style []
-;   {:color "red"
-;    :width "100%"})
+;; -------------------------
+;; Styles
+
+(inject-global
+  {:body     {:background-color "lightblue"
+              :text-align "center"}
+   })
+
+(defstyles component-answer []
+  {:background-color "yellow"
+  :width "40%"
+  :border-radius "20px"
+  :border "3px solid black"
+  :margin "auto"
+  :margin-bottom "30px"
+  :padding "15px"})
 
 
 (defn fetch-data! [data]
-  (GET "https://opentdb.com/api.php?amount=10&category=26&type=multiple"
+  (GET "https://opentdb.com/api.php?amount=50&category=9&type=multiple"
    {:handler #(reset! data %)
      :error-handler (fn [{:keys [status status-text]}]
                       (prn status status-text))}))
@@ -29,19 +44,17 @@
 (let [data (atom nil)]
   (fetch-data! data)
   (fn []
-  (let [{:strs [results]} @data]
-  (prn (first results))
-  (let [{:strs [category type difficulty question correct_answer incorrect_answers]} (first results)]
-
-  (def answers (concat [correct_answer] incorrect_answers))
-  (prn(shuffle answers))
-    [:div 
-    [:div [:h1 "Celebrity Quiz"]]
-    [:div [:h3 question]
-    (for [answers answers]   
-    [:p {:key answers :on-click (fn [] (check-correct answers correct_answer) (fetch-data! data))} answers])]
-    [:h4 "Your score is " @correct-count]
-      ]
+    (let [{:strs [results]} @data]
+    (let [{:strs [category type difficulty question correct_answer incorrect_answers]} (first results)]
+    (def answers (concat [correct_answer] incorrect_answers))
+  
+      [:div 
+      [:div [:h1 "Let's Quiz!"]]
+      [:div [:h3 question]
+      (for [answers (shuffle answers)]   
+      [:p {:key answers :class (component-answer) :on-click (fn [] (check-correct answers correct_answer) (fetch-data! data))} answers])]
+      [:h4 "Your score is " @correct-count]
+        ]
   )
 ))))
 
